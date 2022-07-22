6a555bc1e25e349d59d0183f83d65047436520f2
Vorrathon <neofreedom2539@gmail.com>
2022-07-22 11:18:19 +0700

    comment

--- python3/EP10.py
+++ python3/EP10.py
@@ -3,13 +3,13 @@ x= 5+10*5
 print(x)
 y= (5+10)*5 
 print(y)
-z = (5+2)*10/5
-zz = (5+2)/10*5
-xx = (5*2)/(2/1)
-print(z)
-print(zz)
-print(xx)
-print("Hello world fff")
-print ("Vorrathon")
-print("xxxxxxxx")
-print(x+y)
+# z = (5+2)*10/5
+# zz = (5+2)/10*5
+# xx = (5*2)/(2/1)
+# print(z)
+# print(zz)
+# print(xx)
+# print("Hello world fff")
+# print ("Vorrathon")
+# print("xxxxxxxx")
+# print(x+y)
