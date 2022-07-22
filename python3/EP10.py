f35c5da924eec81a57ef653f24a0a5fa131e83a6
Vorrathon <neofreedom2539@gmail.com>
2022-07-22 15:31:04 +0700

    add-readme

--- python3/EP10.py
+++ python3/EP10.py
@@ -1,7 +1,7 @@
-#ลำดับความสำคัญของตัวดำเนินการ
-x= 5+10*5
+# ลำดับความสำคัญของตัวดำเนินการ
+x = 5+10*5
 print(x)
-y= (5+10)*5 
+y = (5+10)*5
 print(y)
 z = (5+2)*10/5
 zz = (5+2)/10*5
@@ -10,7 +10,7 @@ print(z)
 print(zz)
 print(xx)
 print("Hello world fff")
-print ("Vorrathon")
+print("Vorrathon")
 print("xxxxxxxx")
 print(x+y)
 print(x+y+z)
